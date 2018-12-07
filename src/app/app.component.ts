95b162b002d54a28dbec99f1e54d2578673e47fa
Santosh <santosh.v@idsnext.com>
2018-12-07 10:42:02 +0530

    Code push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { TRANSLATE } from './translate';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  LABEL: any;
+  TR: any;
+  constructor() {
+    this.init();
+  }
+  init(){
+    if (!localStorage.getItem('lang')) {
+      localStorage.setItem("lang", 'en')
+    }
+    let lang: string = localStorage.getItem('lang');
+    this.LABEL = TRANSLATE[lang].label;
+    this.TR = TRANSLATE[lang].sh;
+  }
+  changeLanguage(lang){
+    localStorage.setItem("lang", lang);
+    this.init();
+  }
 }
---
+++ src/app/translate.ts
@@ -0,0 +1,110 @@
+export const TRANSLATE = {
+    "en": {
+        "label": {
+            "born": "Born",
+            "dob": "April 1991",
+            "fatherName": "Father Name",
+            "brotherName": "Brother Name",
+            "sister": "Sister",
+            "height": "Height",
+            "caste": "Caste",
+            "gotra": "Gotra",
+            "mool": "Mool",
+            "address": "Address",
+            "university": "University/Board",
+            "percentage": "Percentage",
+            "division": "Division",
+            "matric": "Matriculation/10th",
+            "isc": "I.Sc",
+            "bsc": "B.Sc"
+
+        },
+        "sh": {
+            "dob": "April 1991",
+            "occ": "Civil Contractor",
+            "occDesc": "He is constructing a building or infrastructure. Construction differs from manufacturing in that manufacturing typically involves mass production.",
+            "name": "Shankar Kumar Vishwakarma",
+            "fatherName": "Late Shiv Balak Vishwakarma",
+            "brotherName": "Santosh Vishwakarma (Senior Software Engineer)",
+            "sister": "	4 (3 Married, 1 UnMarried)",
+            "height": "	5&quot;7",
+            "caste": "Vishwakarma",
+            "gotra": "Sandilya",
+            "mool": "Kishoriya",
+            "address": "AT : Baramuri <br> PO : B-Polytechnic<br> DIST : Dhanbad<br> PIN : 828130<br> Mobile : 9504756595, 9620112955",
+            "matric": {
+                "university": "JAC Ranchi",
+                "percentage": "61.6%",
+                "division": "1st",
+                "year": "June 2006"
+            },
+            "isc": {
+                "university": "JAC Ranchi",
+                "percentage": "58.8%",
+                "division": "2nd",
+                "year": "June 2008"
+            },
+            "bsc": {
+                "university": "VBU Hazaribag",
+                "percentage": "52.6%",
+                "division": "2nd",
+                "year": "June 2011"
+            }
+
+        }
+    },
+    "hn": {
+        "label": {
+            "born": "जन्म",
+            "dob": "अप्रैल 1991",
+            "fatherName": "पिता का नाम",
+            "brotherName": "भाई का नाम",
+            "sister": "बहन",
+            "height": "ऊंचाई",
+            "caste": "जाति",
+            "gotra": "गोत्र",
+            "mool": "मूल",
+            "address": "पता",
+            "university": "विश्वविद्यालय/स्कूल",
+            "percentage": "प्रतिशत",
+            "division": "श्रेणी",
+            "matric": "मैट्रिक",
+            "isc": "आईएससी",
+            "bsc": "बीएससी"
+
+        },
+        "sh": {
+            "dob": "अप्रैल 1991",
+            "occ": "सिविल ठेकेदार",
+            "occDesc": "मेरा काम इमारत या बुनियादी ढांचे का निर्माण करना है। निर्माण में विनिर्माण से भिन्न निर्माण आमतौर पर बड़े पैमाने पर उत्पादन शामिल है।",
+            "name": "शंकर कुमार विश्वकर्मा",
+            "fatherName": "स्वर्गीय शिव बालक विश्वकर्मा",
+            "brotherName": "संतोष विश्वकर्मा (वरिष्ठ सॉफ्टवेयर अभियंता)",
+            "sister": "	4 (3 विवाहित, 1 अविवाहित)",
+            "height": "	5&quot;7",
+            "caste": "विश्वकर्मा",
+            "gotra": "संडीलाया",
+            "mool": "किशोरीया",
+            "address": " ग्राम : बारामुरी <br> पी ओ : बि-पॉली तकनीक निक <br> जिला : धनबाद<br> पिन : 828130<br> मोबाइल : 9504756595, 9620112955",
+            "matric": {
+                "university": "जेएसी रांची",
+                "percentage": "61.6%",
+                "division": "प्रथम",
+                "year": "जून 2006"
+            },
+            "isc": {
+                "university": "जेएसी रांची",
+                "percentage": "58.8%",
+                "division": "सेकंड",
+                "year": "जून 2008"
+            },
+            "bsc": {
+                "university": "वि.बी.यू. हजारीबाग",
+                "percentage": "52.6%",
+                "division": "सेकंड",
+                "year": "जून 2011"
+            }
+
+        }
+    }
+}
